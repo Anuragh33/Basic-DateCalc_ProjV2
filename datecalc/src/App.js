15924faea6aa340648cc0e47f6d02c2974c25721
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-01-07 19:59:39 -0600

    updated files

--- datecalc/src/App.js
+++ datecalc/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./styles.css"
+import { useState } from "react"
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+function Counter() {
+  const [steps, setSteps] = useState(1)
+
+  const [count, setCount] = useState(0)
+
+  const cnt = count * steps
+
+  const date = new Date("january 06 2024")
+  date.setDate(date.getDate() + cnt)
+
+  function increaseCount() {
+    setCount((c) => c + 1)
+  }
+  function decreaseCount() {
+    setCount((c) => c - 1)
+  }
+
+  function reset() {
+    setCount(0)
+    setSteps(1)
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          defaultValue="1"
+          step="1"
+          onChange={(e) => setSteps(Number(e.target.value))}
+        ></input>
+        {steps}
+        {/* <button onClick={decreaseStep}>-</button>
+        Steps :{steps}
+        <button onClick={increaseStep}>+</button> */}
+      </div>
+
+      <div>
+        <button onClick={decreaseCount}>-</button>
+        <input
+          type="text"
+          value={cnt}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
+        <button onClick={increaseCount}>+</button>
+      </div>
+      <div>
+        <span>{cnt === 0 ? "Today is " : ""}</span>
+        <span>{cnt > 0 ? `${cnt} days from now  is ` : ""}</span>
+        <span>{cnt < 0 ? `${cnt} days ago is ` : ""}</span>
+        <span>{date.toDateString()}</span>
+      </div>
+
+      {count !== 0 || steps !== 1 ? (
+        <div>
+          <button onClick={reset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  )
+}
